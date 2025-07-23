aa33314a69d9bcd56f7a081046af29485485c9ee
Sodeso <imsodeso@gmail.com>
2025-07-23 19:35:03 +0300

    1.0.3

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -13,7 +13,10 @@ export default ((opts?: Options) => {
     const links = opts?.links ?? []
     return (
       <footer class={`${displayClass ?? ""}`}>
-        <p>{i18n(cfg.locale).components.footer.createdWith} </p>
+        <p>
+          {i18n(cfg.locale).components.footer.createdWith}{" "}
+          <a href="https://sodeso.github.io/">Инженер-Центр</a>
+        </p>
         <ul>
           {Object.entries(links).map(([text, link]) => (
             <li>
